162c38bfbed57c4ecc94d67f6d9a01aeb33f2504
Jakefawkes <jakefawkess@hotmail.com>
2024-02-27 10:36:40 -0600

    proposed (not working) fix

--- utils.py
+++ utils.py
@@ -51,3 +51,24 @@ def predictive_normal_log_likelihood(y, y_pred_vec, covariance):
     for y_pred in y_pred_vec.T:
         likelihood_list.append(multivariate_normal_likelihood(y, y_pred, covariance))
     return np.log(sum(likelihood_list)/len(likelihood_list))
+
+def log_ratio_calc(y,y_pred_beta,y_pred_beta_samples, covariance):
+    ### This is an attempted fix given what we discussed before, however it still does not seem to work....
+    ### multivariate_normal_log_likelihood(y, y_pred, covariance) - denom_likelihood is still so small that
+    ### The exp of it is too large, I dont know how to get round this apart from increasing covariance
+    """
+    Compute the log likelihood of the posterior predictive  .
+
+    Parameters:
+    - y: y values to be input into likelihood as a numpy array (n), where n is the number of samples.
+    - y_pred_vec: Predictions as a numpy array (n x d), where d is the number of predictions to be averaged over. 
+    - covariance: Covariance matrix of the multivariate normal distribution.
+
+    Returns:
+    - log_predictive_likelihood: Log likelihood of the posterior predictive at y.
+    """
+    likelihood_list = []
+    denom_likelihood = multivariate_normal_log_likelihood(y, y_pred_beta, covariance)
+    for y_pred in y_pred_beta_samples.T:
+        likelihood_list.append( np.exp(multivariate_normal_log_likelihood(y, y_pred, covariance) - denom_likelihood))
+    return -np.log(sum(likelihood_list)/len(likelihood_list))
