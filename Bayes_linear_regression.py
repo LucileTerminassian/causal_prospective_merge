new formula for closed form posterior + fixed an error in bayesian update (fitted the same twice at the candidate site)

Bayes_linear_regression.py
@@ -11,6 +11,20 @@ import pyro.distributions as dist
 from pyro.infer import MCMC, NUTS
 
 
+def posterior_mean(X, y, sigma_sq, S0_inv, m0):
+    
+    A = 1/sigma_sq * np.dot(X.T, X) + S0_inv
+    A_inv = np.linalg.inv(A)
+    b = 1/sigma_sq * np.dot(X.T, y) + np.dot(S0_inv, m0)
+    
+    return np.dot(A_inv, b)
+
+def posterior_covariance(X, sigma_sq, S0_inv):
+    
+    
+    return np.linalg.inv(1/sigma_sq * np.dot(X.T, X) + S0_inv)
+
+
 class BayesianLinearRegression:
 
     def __init__(self, prior_hyperparameters, model='linear_reg'):
@@ -43,14 +57,16 @@ class BayesianLinearRegression:
             raise ValueError("beta_0 should be a vector of length d.")
 
         sigma_sq_y = np.var(Y)
-        sigma_sq_inv_y = 1 / sigma_sq_y
+        # old sigma_sq_inv_y = 1 / sigma_sq_y
         sigma_0_sq_inv = 1 / sigma_0_sq
 
         # Calculate covariance matrix of the posterior distribution
-        cov_matrix_posterior = np.linalg.inv(sigma_sq_inv_y * np.dot(X.T, X) + sigma_0_sq_inv * np.eye(X.shape[1]))
+        # old cov_matrix_posterior = np.linalg.inv(sigma_sq_inv_y * np.dot(X.T, X) + sigma_0_sq_inv * np.eye(X.shape[1]))
+        cov_matrix_posterior = posterior_covariance(X, sigma_sq_y, sigma_0_sq_inv)
 
         # Calculate mean vector of the posterior distribution
-        beta_posterior = np.dot(np.dot(cov_matrix_posterior, X.T), Y) * sigma_sq_inv_y + np.dot(cov_matrix_posterior, beta_0)
+        # old beta_posterior = np.dot(np.dot(cov_matrix_posterior, X.T), Y) * sigma_sq_inv_y + np.dot(cov_matrix_posterior, beta_0)
+        beta_posterior = posterior_mean(X, Y, sigma_sq_y, sigma_0_sq_inv, beta_0)
 
         if sigma_sq_y == 0:
             raise ValueError("Variance of Y is zero. Cannot divide by zero.")

RCT_experiment.py
@@ -113,12 +113,12 @@ def generate_design_matrix(data, power_x, power_x_t):
                 X_prime[f"T*{col}"] = T * (X[col] ** i)
     return X_prime
 
-def add_outcome(data, outcome_function):
+def add_outcome(data, outcome_function, scale):
     
     n = np.shape(data)[0]
     X = data.drop(columns=['T']).values
     T = data['T'].values
-    eps = np.random.normal(size=n)
+    eps = np.random.normal(size=n, scale=scale)
 
     Y = outcome_function(X, T, eps)
     data['Y'] = Y

linear_simulations.py
@@ -73,19 +73,20 @@ if __name__ == '__main__':
     ### Bayesian update through candidate datasets
     sigma_cand = 1
     prior_hyperparameters_cand = {'beta_0': beta_post_host, 'sigma_0_sq': sigma_cand}
-    bayes_reg_cand = BayesianLinearRegression(prior_hyperparameters_cand)
 
     ## With candidate = mirror dataset
+    bayes_reg_mirror = BayesianLinearRegression(prior_hyperparameters_cand)
     X_mirror, Y_mirror = design_data_mirror.drop(columns=['Y']), design_data_mirror['Y']
-    post_mirror_parameters = bayes_reg.fit(X_mirror, Y_mirror)
+    post_mirror_parameters = bayes_reg_mirror.fit(X_mirror, Y_mirror)
 
     # Generate Y_post_mirror
     post_mirror_mean = post_mirror_parameters['posterior_mean'].flatten()  # Extract posterior mean
     Y_post_mirror = np.dot(X_mirror, post_mirror_mean) + np.random.normal(0, 1, len(X_mirror))  # Assuming standard deviation for Y_post_host is 1
 
     ## With candidate = cand2 dataset
+    bayes_reg_cand2 = BayesianLinearRegression(prior_hyperparameters_cand)
     X_cand2, Y_cand2 = design_data_cand2.drop(columns=['Y']), design_data_cand2['Y']
-    post_cand2_parameters = bayes_reg.fit(X_cand2, Y_cand2)
+    post_cand2_parameters = bayes_reg_cand2.fit(X_cand2, Y_cand2)
     
     # Generate Y_post_cand2
     post_cand2_mean = post_cand2_parameters['posterior_mean'].flatten()  # Extract posterior mean