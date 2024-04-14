570c11adec88b00d3d2e6875f4335d3898403546
Desi <desi.r.ivanova@gmail.com>
2024-04-14 20:08:10 +0100

    cleanup forgotten prints etc

--- eig_comp_utils.py
+++ eig_comp_utils.py
@@ -204,23 +204,17 @@ def compute_EIG_obs_closed_form_alt(X, cov_matrix_prior, sigma_rand):
 
 
 def compute_EIG_causal_closed_form(
-    X, cov_matrix_prior, sigma_rand, causal_param_first_index
-):
+    X, cov_matrix_prior, sigma_rand, causal_param_first_index: int
+) -> float:
 
     n_e = len(X)
     num_causal = X.shape[1] - causal_param_first_index
     num_non_causal = causal_param_first_index
 
-    sigma_a = cov_matrix_prior[
-        :causal_param_first_index, :causal_param_first_index
-    ]  # # [number of non-causal; number of non-causal]
-    sigma_b = cov_matrix_prior[
-        causal_param_first_index:, causal_param_first_index:
-    ]  # [number of causal; number of causal]
-
-    sigma_c = cov_matrix_prior[
-        :causal_param_first_index, causal_param_first_index:
-    ]  # [number of non-causal; number of causal]
+    # shapes: [num_nc, num_nc], [num_c, num_c], [num_nc, num_c], respectively
+    sigma_a = cov_matrix_prior[:causal_param_first_index, :causal_param_first_index]
+    sigma_b = cov_matrix_prior[causal_param_first_index:, causal_param_first_index:]
+    sigma_c = cov_matrix_prior[:causal_param_first_index, causal_param_first_index:]
     assert (
         sigma_c.shape == (num_non_causal, num_causal)
         and sigma_a.shape == (num_non_causal, num_non_causal)
@@ -228,21 +222,21 @@ def compute_EIG_causal_closed_form(
     ), "Shape mismatch!"
 
     # I think this is wrong, I think this gives cov_matrix causal?
-    # changing name to cov_matrix_prior_c
-    cov_matrix_prior_c = sigma_b - np.dot(
-        np.dot(sigma_c.T, np.linalg.inv(sigma_a)), sigma_c
-    )  # [number of causal; number of causal]
+    # changing name to cov_matrix_prior_c; also commenting it out
+    # cov_matrix_prior_c = sigma_b - np.dot(
+    #     np.dot(sigma_c.T, np.linalg.inv(sigma_a)), sigma_c
+    # )  # [num_causal, num_causal]
+    # assert cov_matrix_prior_c.shape == (num_causal, num_causal)
 
-    # this should be the non-causal I think... [?]
+    # this should be the non-causal I think... [?!]
     cov_matrix_prior_nc = sigma_a - np.dot(
         np.dot(sigma_c, np.linalg.inv(sigma_b)), sigma_c.T
-    )  # [number of non-causal; number of non-causal]
+    )  # [num_non_causal, num_non_causal]
     assert cov_matrix_prior_nc.shape == (num_non_causal, num_non_causal)
 
     sign, log_gen_term = np.linalg.slogdet(
         X @ ((cov_matrix_prior) @ X.T) + (sigma_rand**2) * np.eye(n_e)
     )  # scalar
-    print("log_gen_term", log_gen_term)
     # phi_nc(X) takes only non-causal columns in X
     phi_nc_X = X[:, :causal_param_first_index]  # [n_e, number of non-causal]
 
