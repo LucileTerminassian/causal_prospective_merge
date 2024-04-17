d771dd8510b429f34af0a814b64e39e50ed62cba
Desi <desi.r.ivanova@gmail.com>
2024-04-17 13:46:18 +0100

    resolve merge

--- research_exp_utils.py
+++ research_exp_utils.py
@@ -10,7 +10,6 @@ from eig_comp_utils import *
 
 ############################ CLOSED FORM
 
-<<<<<<< HEAD
 
 def linear_eig_closed_form_varying_sample_size(
     data: dict[int, dict],
@@ -24,38 +23,6 @@ def linear_eig_closed_form_varying_sample_size(
     # dict of the form {candidate_name: {sample_size: EIG}}
     EIG_obs = {name: [] for name in condidates_names}
     EIG_caus = {name: [] for name in condidates_names}
-=======
-## noisy
-
-
-def linear_eig_closed_form_varying_sample_size(
-    data, data_parameters, sigma_rand_error, prior_hyperparameters, n_mc, verbose=True
-):
-
-    n_both_candidates_list, proportion = (
-        data_parameters["n_both_candidates_list"],
-        data_parameters["proportion"],
-    )
-    std_true_y, causal_param_first_index = (
-        data_parameters["std_true_y"],
-        data_parameters["causal_param_first_index"],
-    )
-
-    results = {
-        "EIG_obs_closed_form_complementary": [],
-        "EIG_obs_closed_form_cand2": [],
-        "EIG_caus_closed_form_complementary": [],
-        "EIG_caus_closed_form_cand2": [],
-    }
-
-    for length in n_both_candidates_list:
-
-        n_both_candidates_list, proportion, causal_param_first_index = (
-            data_parameters["n_both_candidates_list"],
-            data_parameters["proportion"],
-            data_parameters["causal_param_first_index"],
-        )
->>>>>>> main
 
     for length in sample_sizes:
         dlen = data[length]  # for convenience
@@ -63,7 +30,6 @@ def linear_eig_closed_form_varying_sample_size(
         bayes_reg.set_causal_index(data_parameters["causal_param_first_index"])
 
         ### Bayesian update on host data using closed form
-<<<<<<< HEAD
         X_host = torch.from_numpy(dlen["host"].drop(columns=["Y"]).values)
         Y_host = torch.from_numpy(dlen["host"]["Y"].values)
         # fit the posterior (updates the params in the model class)
@@ -82,169 +48,21 @@ def linear_eig_closed_form_varying_sample_size(
             EIG_caus[cand].append(bayes_reg.closed_form_causal_EIG(X_cand))
 
     return EIG_obs, EIG_caus
-=======
-        X_host, Y_host = torch.from_numpy(
-            data[length]["host"].drop(columns=["Y"]).values
-        ), torch.from_numpy(data[length]["host"]["Y"].values)
-        post_host_parameters = bayes_reg.fit(X_host, Y_host)
-
-        X_complementary = torch.from_numpy(
-            data[length]["complementary"].drop(columns=["Y"]).values
-        )
-        X_cand2 = torch.from_numpy(data[length]["cand2"].drop(columns=["Y"]).values)
-
-        if verbose:
-
-            T_host, T_complementary, T_cand2 = (
-                data[length]["host"]["T"],
-                data[length]["complementary"]["T"],
-                data[length]["cand2"]["T"],
-            )
-
-            percent_treated_host = 100 * sum(T_host) / len(T_host)
-            percent_treated_complementaryr = (
-                100 * sum(T_complementary) / len(T_complementary)
-            )
-            percent_treated_cand2 = 100 * sum(T_cand2) / len(T_cand2)
-
-            print("For a sample size in complementary and host of " + str(length))
-            print(f"Percentage of treated in host: {percent_treated_host}%")
-            print(
-                f"Percentage of treated in complementary: {percent_treated_complementaryr}%"
-            )
-            print(f"Percentage of treated in cand2: {percent_treated_cand2}%")
-
-        results["EIG_obs_closed_form_complementary"].append(
-            bayes_reg.closed_form_obs_EIG(X_complementary)
-        )
-        results["EIG_obs_closed_form_cand2"].append(
-            bayes_reg.closed_form_obs_EIG(X_cand2)
-        )
-
-        results["EIG_caus_closed_form_complementary"].append(
-            bayes_reg.closed_form_causal_EIG(X_complementary)
-        )
-        results["EIG_caus_closed_form_cand2"].append(
-            bayes_reg.closed_form_causal_EIG(X_cand2)
-        )
-
-    EIG_obs_closed_form = np.vstack(
-        (
-            results["EIG_obs_closed_form_complementary"],
-            results["EIG_obs_closed_form_cand2"],
-        )
-    )
-    EIG_caus_closed_form = np.vstack(
-        (
-            results["EIG_caus_closed_form_complementary"],
-            results["EIG_caus_closed_form_cand2"],
-        )
-    )
-
-    return EIG_obs_closed_form, EIG_caus_closed_form
-
-
-## exact
-
-
-def linear_eig_closed_form_exact_datasets(
-    data, data_parameters, sigma_rand_error, prior_hyperparameters, n_mc
-):
-
-    n_both_candidates_list, proportion = (
-        data_parameters["n_both_candidates_list"],
-        data_parameters["proportion"],
-    )
-    std_true_y, causal_param_first_index = (
-        data_parameters["std_true_y"],
-        data_parameters["causal_param_first_index"],
-    )
-
-    dict_results_obs = {
-        "complementary": [],
-        "twin": [],
-        "twin_treated": [],
-        "twin_untreated": [],
-    }
-    dict_results_caus = {
-        "complementary": [],
-        "twin": [],
-        "twin_treated": [],
-        "twin_untreated": [],
-    }
-
-    for length in n_both_candidates_list:
-
-        bayes_reg = BayesianLinearRegression(prior_hyperparameters)
-        bayes_reg.set_causal_index(causal_param_first_index)
-
-        ### Bayesian update on host data using closed form
-        X_host, Y_host = torch.from_numpy(
-            data[length]["host"].drop(columns=["Y"]).values
-        ), torch.from_numpy(data[length]["host"]["Y"].values)
-        post_host_parameters = bayes_reg.fit(X_host, Y_host)
-
-        X_exact_complementary = torch.from_numpy(
-            data[length]["exact_complementary"].drop(columns=["Y"]).values
-        )
-        X_exact_twin = torch.from_numpy(
-            data[length]["exact_twin"].drop(columns=["Y"]).values
-        )
-        X_exact_twin_treated = torch.from_numpy(
-            data[length]["exact_twin_treated"].drop(columns=["Y"]).values
-        )
-        X_exact_twin_untreated = torch.from_numpy(
-            data[length]["exact_twin_untreated"].drop(columns=["Y"]).values
-        )
-
-        dict_results_obs["complementary"].append(
-            bayes_reg.closed_form_obs_EIG(X_exact_complementary)
-        )
-        dict_results_obs["twin"].append(bayes_reg.closed_form_obs_EIG(X_exact_twin))
-        dict_results_obs["twin_treated"].append(
-            bayes_reg.closed_form_obs_EIG(X_exact_twin_treated)
-        )
-        dict_results_obs["twin_untreated"].append(
-            bayes_reg.closed_form_obs_EIG(X_exact_twin_untreated)
-        )
-
-        dict_results_caus["complementary"].append(
-            bayes_reg.closed_form_causal_EIG(X_exact_complementary)
-        )
-        dict_results_caus["twin"].append(bayes_reg.closed_form_causal_EIG(X_exact_twin))
-        dict_results_caus["twin_treated"].append(
-            bayes_reg.closed_form_causal_EIG(X_exact_twin_treated)
-        )
-        dict_results_caus["twin_untreated"].append(
-            bayes_reg.closed_form_causal_EIG(X_exact_twin_untreated)
-        )
-
-    return dict_results_obs, dict_results_caus
->>>>>>> main
 
 
 #################################### FROM SAMPLES
 ## noisy
 
 
-<<<<<<< HEAD
 # What's the difference with the above?
-=======
->>>>>>> main
 def linear_eig_from_samples_varying_sample_size(
     data, data_parameters, prior_hyperparameters, sampling_parameters
 ):
 
     results = {
-<<<<<<< HEAD
-        "EIG_obs_from_samples_mirror": [],
-        "EIG_obs_from_samples_cand2": [],
-        "EIG_caus_from_samples_mirror": [],
-=======
         "EIG_obs_from_samples_complementary": [],
         "EIG_obs_from_samples_cand2": [],
         "EIG_caus_from_samples_complementary": [],
->>>>>>> main
         "EIG_caus_from_samples_cand2": [],
     }
 
@@ -274,13 +92,9 @@ def linear_eig_from_samples_varying_sample_size(
         X_host, Y_host = torch.from_numpy(
             data[length]["host"].drop(columns=["Y"]).values
         ), torch.from_numpy(data[length]["host"]["Y"].values)
-<<<<<<< HEAD
-        X_mirror = torch.from_numpy(data[length]["mirror"].drop(columns=["Y"]).values)
-=======
         X_complementary = torch.from_numpy(
             data[length]["complementary"].drop(columns=["Y"]).values
         )
->>>>>>> main
         X_cand2 = torch.from_numpy(data[length]["cand2"].drop(columns=["Y"]).values)
 
         bayes_reg = BayesianLinearRegression(prior_hyperparameters)
@@ -291,17 +105,11 @@ def linear_eig_from_samples_varying_sample_size(
         n_samples = n_samples_outer_expectation * (n_samples_inner_expectation + 1)
         beta_post_host_samples = bayes_reg.posterior_sample(n_samples=n_samples)
 
-<<<<<<< HEAD
-        results["EIG_obs_from_samples_mirror"].append(
-            bayes_reg.samples_obs_EIG(
-                X_mirror, n_samples_outer_expectation, n_samples_inner_expectation
-=======
         results["EIG_obs_from_samples_complementary"].append(
             bayes_reg.samples_obs_EIG(
                 X_complementary,
                 n_samples_outer_expectation,
                 n_samples_inner_expectation,
->>>>>>> main
             )
         )
         results["EIG_obs_from_samples_cand2"].append(
@@ -310,15 +118,9 @@ def linear_eig_from_samples_varying_sample_size(
             )
         )
 
-<<<<<<< HEAD
-        results["EIG_caus_from_samples_mirror"].append(
-            bayes_reg.samples_causal_EIG(
-                X_mirror, n_causal_outer_exp, n_causal_inner_exp
-=======
         results["EIG_caus_from_samples_complementary"].append(
             bayes_reg.samples_causal_EIG(
                 X_complementary, n_causal_outer_exp, n_causal_inner_exp
->>>>>>> main
             )
         )
         results["EIG_caus_from_samples_cand2"].append(
@@ -328,13 +130,6 @@ def linear_eig_from_samples_varying_sample_size(
         )
 
     EIG_obs_samples = np.vstack(
-<<<<<<< HEAD
-        (results["EIG_obs_from_samples_mirror"], results["EIG_obs_from_samples_cand2"])
-    )
-    EIG_caus_samples = np.vstack(
-        (
-            results["EIG_caus_from_samples_mirror"],
-=======
         (
             results["EIG_obs_from_samples_complementary"],
             results["EIG_obs_from_samples_cand2"],
@@ -343,7 +138,6 @@ def linear_eig_from_samples_varying_sample_size(
     EIG_caus_samples = np.vstack(
         (
             results["EIG_caus_from_samples_complementary"],
->>>>>>> main
             results["EIG_caus_from_samples_cand2"],
         )
     )
@@ -488,15 +282,9 @@ def bart_eig_from_samples_varying_sample_size(
     # data[length]['host] is a pandas df for the host site
 
     results = {
-<<<<<<< HEAD
-        "EIG_obs_from_samples_mirror": [],
-        "EIG_obs_from_samples_cand2": [],
-        "EIG_caus_from_samples_mirror": [],
-=======
         "EIG_obs_from_samples_complementary": [],
         "EIG_obs_from_samples_cand2": [],
         "EIG_caus_from_samples_complementary": [],
->>>>>>> main
         "EIG_caus_from_samples_cand2": [],
     }
 
@@ -515,17 +303,10 @@ def bart_eig_from_samples_varying_sample_size(
         n_causal_inner_exp,
         n_causal_outer_exp,
     ) = (
-<<<<<<< HEAD
-        sampling_parameters["n_samples_outer_expectation"],
-        sampling_parameters["n_samples_inner_expectation"],
-        sampling_parameters["n_causal_inner_exp"],
-        sampling_parameters["n_causal_outer_exp"],
-=======
         sampling_parameters["n_samples_outer_expectation_obs"],
         sampling_parameters["n_samples_inner_expectation_obs"],
         sampling_parameters["n_samples_inner_expectation_caus"],
         sampling_parameters["n_samples_outer_expectation_caus"],
->>>>>>> main
     )
 
     for length in n_both_candidates_list:
@@ -536,15 +317,6 @@ def bart_eig_from_samples_varying_sample_size(
             data[length]["host"]["Y"].values,
         )
 
-<<<<<<< HEAD
-        X_mirror, T_mirror = (
-            data[length]["mirror"].drop(columns=["Y", "T"]).values,
-            data[length]["mirror"]["T"].values,
-        )
-        X_cand2, T_cand2 = (
-            data[length]["cand2"].drop(columns=["Y", "T"]).values,
-            data[length]["mirror"]["T"].values,
-=======
         X_complementary, T_complementary = (
             data[length]["complementary"].drop(columns=["Y", "T"]).values,
             data[length]["complementary"]["T"].values,
@@ -558,7 +330,6 @@ def bart_eig_from_samples_varying_sample_size(
             T_host.astype(np.int32),
             T_complementary.astype(np.int32),
             T_cand2.astype(np.int32),
->>>>>>> main
         )
 
         bcf = BayesianCausalForest(
@@ -568,24 +339,6 @@ def bart_eig_from_samples_varying_sample_size(
         )
         bcf.store_train_data(X=X_host, T=T_host, Y=Y_host)
 
-<<<<<<< HEAD
-        results_mirror = bcf.joint_EIG_calc(X_mirror, T_mirror, sampling_parameters)
-
-        results_cand2 = bcf.joint_EIG_calc(X_cand2, T_cand2, sampling_parameters)
-
-        results["EIG_obs_from_samples_mirror"].append(results_mirror["Obs EIG"])
-        results["EIG_obs_from_samples_cand2"].append(results_cand2["Obs EIG"])
-
-        results["EIG_caus_from_samples_mirror"].append(results_mirror["Causal EIG"])
-        results["EIG_caus_from_samples_cand2"].append(results_cand2["Causal EIG"])
-
-    EIG_obs_samples = np.vstack(
-        (results["EIG_obs_from_samples_mirror"], results["EIG_obs_from_samples_cand2"])
-    )
-    EIG_caus_samples = np.vstack(
-        (
-            results["EIG_caus_from_samples_mirror"],
-=======
         results_complementary = bcf.joint_EIG_calc(
             X_complementary, T_complementary, sampling_parameters
         )
@@ -611,7 +364,6 @@ def bart_eig_from_samples_varying_sample_size(
     EIG_caus_samples = np.vstack(
         (
             results["EIG_caus_from_samples_complementary"],
->>>>>>> main
             results["EIG_caus_from_samples_cand2"],
         )
     )
@@ -669,11 +421,7 @@ def bart_eig_from_samples_exact_datasets(
 
         X_exact_complementary, T_exact_complementary = (
             data[length]["exact_complementary"].drop(columns=["Y", "T"]).values,
-<<<<<<< HEAD
-            data[length]["host"]["T"].values,
-=======
             data[length]["exact_complementary"]["T"].values,
->>>>>>> main
         )
         X_exact_twin, T_exact_twin = (
             data[length]["exact_twin"].drop(columns=["Y", "T"]).values,
@@ -734,7 +482,7 @@ if __name__ == "__main__":
     from rct_data_generator import _main
 
     _, data_parameters = _main()
-    exact_data = generate_exact_synthetic_data_varying_sample_size(data_parameters)
+    exact_data = generate_exact_data_varying_sample_size(data_parameters)
 
     sigma_prior = 1.0
     sigma_rand_error = 1.0
